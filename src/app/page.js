3e6d04033030702d9660c9a903b2c3e5b1f93373
John <johnolshea@gmail.com>
2024-04-09 13:30:08 +0100

    Add Description section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 'use client';
 import Landing from '../components/Landing'
+import Description from '../components/Description'
 
 import styles from './page.module.scss'
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Landing />
+      <Description />
     </main>
   )
 }
